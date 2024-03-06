a470d023a5bbbd2160aef1ab3a76d57d58eaa554
Szxro <szxro@gmail.com>
2024-03-06 19:55:41 -0400

    Add base entity and auditable entity

---
+++ Clinic.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Clinic.Domain.Common;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? ModifiedAt { get; set;}
+}
