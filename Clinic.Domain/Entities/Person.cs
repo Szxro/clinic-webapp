257ca8164a85439df5251b99ce8d7973d9d05310
Szxro <szxro@gmail.com>
2024-03-06 19:56:34 -0400

    Add entities folder and person entity

---
+++ Clinic.Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class Person : AuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Telephone { get; set; } = string.Empty;
+
+    public string NIF { get; set; } = string.Empty;
+
+    public int SocialNumber { get; set; }
+}
