77d268d835e2dcee1e3d020755960cf775c384d4
Szxro <szxro@gmail.com>
2024-03-06 20:47:25 -0400

    Add doctor position entity

---
+++ Clinic.Domain/Entities/DoctorPosition.cs
@@ -0,0 +1,10 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class DoctorPosition : AuditableEntity
+{
+    public string PositionName { get; set; } = string.Empty;
+
+    public Doctor Doctor { get; set; } = new();
+}
