1ed16fee28fe9952ff9fac3aa9eb5e3780df24ca
Szxro <szxro@gmail.com>
2024-03-06 20:48:46 -0400

    Add patient entity

---
+++ Clinic.Domain/Entities/Patient.cs
@@ -0,0 +1,17 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class Patient : AuditableEntity
+{
+    public Patient()
+    {
+        DoctorPatients = new HashSet<DoctorPatient>();
+    }
+
+    public Person Person { get; set; } = new();
+
+    public int PersonId { get; set; }
+
+    public ICollection<DoctorPatient> DoctorPatients { get; set; } 
+}
