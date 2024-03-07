ef91dcdf3efd69288fcc6e0f7eb79cdc27a24ba2
Szxro <szxro@gmail.com>
2024-03-06 20:48:19 -0400

    Add employee position entity

---
+++ Clinic.Domain/Entities/EmployeePosition.cs
@@ -0,0 +1,10 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class EmployeePosition : AuditableEntity
+{
+    public string PositionaName { get; set; } = string.Empty;
+
+    public Employee Employee { get; set; } = new();
+}
