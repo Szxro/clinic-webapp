0021269fde8962aea1def04c74450919d43b0bf4
Szxro <szxro@gmail.com>
2024-03-06 20:45:42 -0400

    Add doctor consult entity

---
+++ Clinic.Domain/Entities/DoctorConsult.cs
@@ -0,0 +1,14 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class DoctorConsult : AuditableEntity
+{
+    public Doctor Doctor { get; set; } = new();
+
+    public int DoctorId { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public DateTime Hour { get; set; }
+}
