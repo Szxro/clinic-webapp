530b7704a972bc2872adb8d03583440e65c5e6b5
Szxro <szxro@gmail.com>
2024-03-06 20:49:17 -0400

    Add person address entity

---
+++ Clinic.Domain/Entities/PersonAddress.cs
@@ -0,0 +1,24 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class PersonAddress : AuditableEntity
+{
+    public int StreerNumber { get; set; }
+
+    public string AddressLine1 { get; set; } = string.Empty;
+
+    public string AddressLine2 { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public int PostalCode { get; set; }
+
+    public int Population { get; set; }
+
+    public string Province { get; set; } = string.Empty;
+
+    public Person Person { get; set; } = new();
+
+    public int PersonId { get; set; }
+}
