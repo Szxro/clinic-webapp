8d5028aa34c34d05819c4dd7ece6f02ca006ebb1
Szxro <szxro@gmail.com>
2024-03-06 20:50:18 -0400

    Add vacation period status entity

---
+++ Clinic.Domain/Entities/VacationPeriodStatus.cs
@@ -0,0 +1,15 @@
+﻿using Clinic.Domain.Common;
+
+namespace Clinic.Domain.Entities;
+
+public class VacationPeriodStatus : AuditableEntity
+{
+    public VacationPeriodStatus()
+    {
+        PersonVacationPeriods = new HashSet<PersonVacationPeriod>();
+    }
+
+    public string StatusName { get; set; } = string.Empty;
+
+    public ICollection<PersonVacationPeriod> PersonVacationPeriods { get; set; }
+}
