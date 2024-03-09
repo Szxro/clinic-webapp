1efa8c90a000c99d5973e4a3cf6ead655607899d
Szxro <szxro@gmail.com>
2024-03-09 19:31:45 -0400

    Add DatabaseOptions validator

---
+++ Clinic.Data/Services/DatabaseOptionsServiceValidator.cs
@@ -0,0 +1,35 @@
+﻿using Clinic.Data.Options;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Clinic.Data.Services;
+
+public sealed class DatabaseOptionsServiceValidator : IValidateOptions<DatabaseOptions>
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public DatabaseOptionsServiceValidator(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public ValidateOptionsResult Validate(string? name, DatabaseOptions options)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+
+        IValidator<DatabaseOptions> validator = scope.ServiceProvider.GetRequiredService<IValidator<DatabaseOptions>>();
+
+        ValidationResult result = validator.Validate(options);
+
+        if (result.IsValid)
+        {
+            return ValidateOptionsResult.Success;
+        }
+
+        var errors = result.Errors.Select(x => $"Validation failed to {x.PropertyName} with the Error Message {x.ErrorMessage}").ToList();
+
+        return ValidateOptionsResult.Fail(errors);
+    }
+}
