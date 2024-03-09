212636caab3ceb7e7f156b0c25474534f2c4d6eb
Szxro <szxro@gmail.com>
2024-03-09 19:30:09 -0400

    Add unit of work interface

---
+++ Clinic.Data/Contracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Data.Contracts;
+
+public interface IUnitOfWork
+{
+    void ChangeContextTracker(object entity, EntityState entityState);
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
