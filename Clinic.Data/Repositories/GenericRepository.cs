440ee6bc6437047fae61d6248b88b65607f66152
Szxro <szxro@gmail.com>
2024-03-09 16:54:18 -0400

    Add repositories folder and generic repository class

---
+++ Clinic.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Clinic.Data.Common;
+using Clinic.Data.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Clinic.Data.Repositories;
+
+public abstract class GenericRepository<TEntity> 
+    where TEntity : BaseEntity
+{
+    protected readonly AppDbContext _dbContext;
+
+    public GenericRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Add(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Add(entity);
+    }
+
+    public void AddRange(ICollection<TEntity> entities)
+    {
+        _dbContext.Set<TEntity>().AddRange(entities);
+    }
+
+    public void ChangeContextTracker(object entity,EntityState entityState)
+    {
+        _dbContext.Entry(entity).State = entityState;
+    }
+
+    public async Task<TEntity?> GetById(int Id)
+    {
+        return await _dbContext.Set<TEntity>().Where(x => x.Id == Id).FirstOrDefaultAsync();
+    }
+
+    public async Task<int?> DeleteBy(Expression<Func<TEntity,bool>> filter)
+    {
+        return await _dbContext.Set<TEntity>().Where(filter).ExecuteDeleteAsync();
+    }
+
+    public async Task<int?> UpdateBy(Expression<Func<TEntity, bool>> filter,
+                                     Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> propertiesExpression)
+    {
+        return await _dbContext.Set<TEntity>().Where(filter).ExecuteUpdateAsync(propertiesExpression);
+    }
+}
