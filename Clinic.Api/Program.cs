000cf1e660c9b35b792a86b63d22c5f6024b6b97
Szxro <szxro@gmail.com>
2024-03-16 15:14:09 -0400

    Add Businnes Layer to the Program.cs

--- Clinic.Api/Program.cs
+++ Clinic.Api/Program.cs
@@ -1,9 +1,11 @@
+using Clinic.Business;
 using Clinic.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
-    builder.Services.AddDataLayer(builder.Configuration,builder.Environment);
+    builder.Services.AddDataLayer(builder.Configuration, builder.Environment);
+    builder.Services.AddBusinnessLayer();
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
