9b46942d16379e70a0d47fc0c093bd5ddb7485db
Szxro <szxro@gmail.com>
2024-03-16 15:11:51 -0400

    Add exception folder and validation exception for the validation pipeline

---
+++ Clinic.Business/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,22 @@
+﻿using Clinic.Data.Entities.Common.Primitives;
+
+namespace Clinic.Business.Common.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException()
+        : base("One or more validation failures have occurred.")
+    {
+        Errors = new Dictionary<string, Error[]>();       
+    }
+    public IDictionary<string, Error[]> Errors { get; }
+
+    public ValidationException(Error[] validationErrors)
+        : this()
+    {
+        Errors = new Dictionary<string, Error[]>()
+        {
+            {"validationErrors", validationErrors}
+        };
+    }
+}
