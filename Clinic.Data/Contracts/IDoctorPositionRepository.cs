0e9f00da6b97562a2be7257b2776c014abd22ee5
Szxro <szxro@gmail.com>
2024-03-16 14:54:44 -0400

    Add interface and repository to the DoctorPosition entity

---
+++ Clinic.Data/Contracts/IDoctorPositionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Clinic.Data.Entities;
+
+namespace Clinic.Data.Contracts;
+
+public interface IDoctorPositionRepository
+{
+    Task AddDefaultDoctorPosition();
+
+    Task<DoctorPosition?> GetDoctorPositionByPositionName(string positionName);
+}
---
+++ Clinic.Data/Repositories/DoctorPositionRepository.cs
@@ -0,0 +1,38 @@
+﻿using Clinic.Data.Common;
+using Clinic.Data.Contracts;
+using Clinic.Data.Entities;
+using Clinic.Data.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Data.Repositories;
+
+public class DoctorPositionRepository : GenericRepository<DoctorPosition>, IDoctorPositionRepository
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DoctorPositionRepository(AppDbContext dbContext, IUnitOfWork unitOfWork) : base(dbContext)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task AddDefaultDoctorPosition()
+    {
+        ICollection<DoctorPosition> doctorPositions = new HashSet<DoctorPosition>()
+        {
+             new DoctorPosition() {PositionName = "Medico Titular" },
+             new DoctorPosition() {PositionName = "Medico Interino" },
+             new DoctorPosition() {PositionName = "Medico Sustituto"}
+        };
+
+        AddRange(doctorPositions);
+
+        await _unitOfWork.SaveChangesAsync();
+    }
+
+    public async Task<DoctorPosition?> GetDoctorPositionByPositionName(string positionName)
+    {
+        return await _dbContext.DoctorPosition
+                               .Where(position => position.PositionName == positionName)
+                               .FirstOrDefaultAsync();
+    }
+}
