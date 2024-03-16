2eb45279b0bb37e3fba021495e9f49f7f5072383
Szxro <szxro@gmail.com>
2024-03-16 15:03:16 -0400

    Change the location of the DatabaseOptionsValidator

---
+++ Clinic.Data/Validators/DatabaseOptionsValidator.cs
@@ -0,0 +1,30 @@
+﻿using Clinic.Data.Options;
+using FluentValidation;
+
+namespace Clinic.Data.Validators;
+
+public class DatabaseOptionsValidator : AbstractValidator<DatabaseOptions>
+{
+    public DatabaseOptionsValidator()
+    {
+        RuleFor(options => options.ConnectionString).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.ConnectionString).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.CommandTimeout).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.CommandTimeout).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.EnableDetailedErrors).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.EnableDetailedErrors).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.EnableSensitiveDataLogging).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.EnableSensitiveDataLogging).NotNull().WithMessage("The {PropertyName} cant be null");
+
+        RuleFor(options => options.MaxRetryCount).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(options => options.MaxRetryCount).NotNull().WithMessage("The {PropertyName} cant be null");
+    }
+}
