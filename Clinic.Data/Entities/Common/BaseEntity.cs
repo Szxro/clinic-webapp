3a9cbd591c6d39bbd23e82119c51c5fd034420d7
Szxro <szxro@gmail.com>
2024-03-16 14:47:41 -0400

    Add event methods and list of events for the entities

--- Clinic.Data/Entities/Common/BaseEntity.cs
+++ Clinic.Data/Entities/Common/BaseEntity.cs
@@ -1,6 +1,27 @@
-﻿namespace Clinic.Data.Entities.Common;
+﻿using Clinic.Data.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clinic.Data.Entities.Common;
 
 public abstract class BaseEntity
 {
     public int Id { get; set; }
+
+    [NotMapped]
+    protected readonly List<IEvent> events = new();
+
+    public void AddEvent(IEvent @event)
+    {
+        events.Add(@event);
+    }
+
+    public void RemoveEvent(IEvent @event)
+    {
+        events.Remove(@event);
+    }
+
+    public void Clear()
+    {
+        events.Clear();
+    }
 }
