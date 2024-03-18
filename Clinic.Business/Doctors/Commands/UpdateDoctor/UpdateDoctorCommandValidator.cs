628ac71b8a42beab67306b2c33785516ffa31215
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-03-18 19:10:02 -0400

    Created DoctorCommand for update

---
+++ Clinic.Business/Doctors/Commands/UpdateDoctor/UpdateDoctorCommandValidator.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using System.Globalization;
+
+
+namespace Clinic.Business.Doctors.Commands.UpdateDoctor
+{
+    public class DoctorCommandValidator : AbstractValidator<UpdateDoctorCommand>
+    {
+        public DoctorCommandValidator()
+        {
+            RuleFor(c => c.doctorId)
+            .GreaterThan(0)
+            .WithMessage("The property {PropertyName} must be above zero.");
+
+            RuleFor(c => c.name)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The property {PropertyName} cannot be empty.");
+
+            RuleFor(c => c.telephone)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The property {PropertyName} cannot be empty.");
+
+            RuleFor(c => c.nif)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The property {PropertyName} cannot be empty.");
+
+            RuleFor(c => c.socialNumber)
+                .GreaterThan(0)
+                .WithMessage("The property {PropertyName} must be above zero.");
+
+            RuleFor(c => c.collegueNumber)
+                .GreaterThan(0)
+                .WithMessage("The property {PropertyName} must be above zero.");
+
+            RuleFor(c => c.startDate)
+               .Must(startDate => DateTime.TryParse(startDate, DateTimeFormatInfo.CurrentInfo, out DateTime _))
+               .When(c => !string.IsNullOrEmpty(c.startDate))
+               .WithMessage("The {PropertyName} is not a valid date");
+
+            RuleFor(c => c.doctorPosition)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The property {PropertyName} cannot be empty.");
+        }
+    }
+}
