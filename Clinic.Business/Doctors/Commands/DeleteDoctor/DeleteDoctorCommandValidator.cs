7739ae1ff82b18a8a8809cd85a450cfdd3cfa47e
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-03-19 21:39:31 -0400

    Create DeleteDoctorCommandValidator

---
+++ Clinic.Business/Doctors/Commands/DeleteDoctor/DeleteDoctorCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Business.Doctors.Commands.DeleteDoctor
+{
+    public class DeleteDoctorCommandValidator: AbstractValidator<DeleteDoctorCommand>
+    {
+        public DeleteDoctorCommandValidator() 
+        { 
+            RuleFor(x => x.DoctorId)
+                .GreaterThan(0)
+                .WithMessage("The property {PropertyName} must be above zero.");
+        
+        }
+
+    }
+}
