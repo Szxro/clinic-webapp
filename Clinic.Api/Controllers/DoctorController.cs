0e2e9d52a2d80af2d22f6533ff57c3c18aaee988
Szxro <szxro@gmail.com>
2024-03-23 12:29:35 -0400

    Add doctor and doctor position controller

---
+++ Clinic.Api/Controllers/DoctorController.cs
@@ -0,0 +1,63 @@
+﻿using Clinic.Api.Extensions;
+using Clinic.Business.Doctors.Commands.CreateDoctor;
+using Clinic.Business.Doctors.Commands.DeleteDoctor;
+using Clinic.Business.Doctors.Commands.UpdateDoctor;
+using Clinic.Business.Doctors.Query.GetDoctorsInformation;
+using Clinic.Data.DTOs;
+using Clinic.Data.Entities.Common.Primitives;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DoctorController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public DoctorController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost("doctor/create")]
+
+    public async Task<ActionResult> CreateDoctor(CreateDoctorCommand createDoctor)
+    {
+        Result result = await _sender.Send(createDoctor);
+
+        return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+    }
+
+    [HttpDelete("doctor/remove")]
+
+    public async Task<ActionResult> DeleteDoctor(DeleteDoctorCommand deleteDoctor)
+    {
+        Result result = await _sender.Send(deleteDoctor);
+
+        return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+    }
+
+    [HttpPut("doctor/update")]
+
+    public async Task<ActionResult> UpdateDoctor(UpdateDoctorCommand updateDoctor)
+    {
+        Result result = await _sender.Send(updateDoctor);
+
+        return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+    }
+
+    [HttpGet("doctor/all")]
+
+    public async Task<ActionResult<PagedList<DoctorDto>>> GetDoctorsInformation(string? name,
+                                                                           string? sortColumn,
+                                                                           string? sortOrder,
+                                                                           int page,
+                                                                           int pageSize)
+    {
+        Result<PagedList<DoctorDto>> result = await _sender.Send(new GetDoctorsInformationQuery(name,sortColumn,sortOrder,page,pageSize));
+
+        return result.IsSuccess ? Ok(result.Data) : result.ToProblemDetails();
+    }
+}
---
+++ Clinic.Api/Controllers/DoctorPositionController.cs
@@ -0,0 +1,44 @@
+﻿using Clinic.Api.Extensions;
+using Clinic.Business.DoctorsPosition.Commands.CreateDoctorPosition;
+using Clinic.Business.DoctorsPosition.Query.GetAllDoctorPositions;
+using Clinic.Data.DTOs;
+using Clinic.Data.Entities.Common.Primitives;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorPositionController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public DoctorPositionController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("doctorposition/create")]
+
+        public async Task<ActionResult> CreateDoctorPosition(CreateDoctorPositionCommand createDoctorPositionCommand)
+        {
+            Result result = await _sender.Send(createDoctorPositionCommand);
+
+            return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+        }
+
+        [HttpGet("doctorposition/all")]
+
+        public async Task<ActionResult<PagedList<DoctorPositionDTO>>> GetAllDoctorsPosition(string? name,
+                                                                           string? sortColumn,
+                                                                           string? sortOrder,
+                                                                           int page,
+                                                                           int pageSize)
+        {
+            Result<PagedList<DoctorPositionDTO>> result = await _sender.Send(new GetAllDoctorPositionsQuery(name, sortColumn, sortOrder, page, pageSize));
+
+            return result.IsSuccess ? Ok(result.Data) : result.ToProblemDetails();
+        }
+    }
+}
