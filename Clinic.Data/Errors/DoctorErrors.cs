669958d540f3c5e8eb4fee2f1bd5220ffd44ba2a
Szxro <szxro@gmail.com>
2024-03-23 12:19:26 -0400

    Add some new errors to the DoctorErrors class

--- Clinic.Data/Errors/DoctorErrors.cs
+++ Clinic.Data/Errors/DoctorErrors.cs
@@ -7,5 +7,15 @@ namespace Clinic.Data.Errors
     {
         public static Error NotFoundById(int doctorId) 
             => Error.NotFound("Doctor.NotFoundById",$"The doctor with the id of {doctorId} was not found");
+
+        public static Error CollegueNumberNotUnique(int collegueNumber)
+            => Error.Conflit("Doctor.CollegueNumberConflit",$"The doctor collegue number {collegueNumber} already exists");
+
+        public static Error NotFoundByName(string doctorName)
+            => Error.NotFound("Doctor.NotFoundById", $"The doctor with the id of {doctorName} was not found");
+
+        public static readonly Error NotFoundDoctors 
+            = Error.NotFound("Doctor.NotFound","Currently there are not doctors register");
+
     }
 }
