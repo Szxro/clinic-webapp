745aedc1fa0b43a03ed66bf7a311718aa0c9d347
Szxro <szxro@gmail.com>
2024-03-23 12:32:24 -0400

    Add errors for the person entity

---
+++ Clinic.Data/Errors/PersonErrors.cs
@@ -0,0 +1,12 @@
+﻿using Clinic.Data.Entities.Common.Primitives;
+
+namespace Clinic.Data.Errors;
+
+public static class PersonErrors
+{
+    public static readonly Error NifNotUnique 
+        = Error.Conflit("Person.NifNotUnique","The NIF is already register");
+
+    public static readonly Error SocialNumberNotUnique
+        = Error.Conflit("Person.SocialNumber", "The Social Number is already register");
+}
