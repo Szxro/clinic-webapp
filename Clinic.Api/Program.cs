a581287bebca57f464d1a57d64c54c45fed2fc76
Szxro <szxro@gmail.com>
2024-03-23 11:42:44 -0400

    Make some adjustments to the Program.cs and add cors policies

--- Clinic.Api/Program.cs
+++ Clinic.Api/Program.cs
@@ -4,13 +4,14 @@ using Clinic.Data;
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
-    builder.Services.AddDataLayer(builder.Configuration, builder.Environment);
+    builder.Services.AddDataLayer(builder.Environment);
     builder.Services.AddBusinnessLayer();
     builder.Services.AddControllers();
+    builder.Services.AddCors();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-    builder.Configuration.AddUserSecrets<Program>(optional:true,reloadOnChange:true);
+    builder.Configuration.AddUserSecrets<Program>(optional:false,reloadOnChange:true);
 }
 
 
@@ -27,6 +28,13 @@ var app = builder.Build();
     }
 
     app.UseHttpsRedirection();
+
+    app.UseCors(options => 
+    {
+        options.AllowAnyHeader();
+        options.AllowAnyMethod();
+        options.AllowAnyOrigin();
+    });
 
     app.UseAuthorization();
 
