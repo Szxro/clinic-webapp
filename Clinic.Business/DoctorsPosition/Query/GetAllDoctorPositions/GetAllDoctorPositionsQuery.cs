2455cd1c6f3c7067519c55a5a49b2893450695a4
Szxro <szxro@gmail.com>
2024-03-23 12:38:44 -0400

    Add query to get all the doctor position with offset pagination

---
+++ Clinic.Business/DoctorsPosition/Query/GetAllDoctorPositions/GetAllDoctorPositionsQuery.cs
@@ -0,0 +1,28 @@
+﻿using Clinic.Data.Contracts;
+using Clinic.Data.DTOs;
+using Clinic.Data.Entities.Common.Primitives;
+
+namespace Clinic.Business.DoctorsPosition.Query.GetAllDoctorPositions;
+
+public record GetAllDoctorPositionsQuery(string? name,
+                                         string? sortColumn,
+                                         string? sortOrder,
+                                         int page,
+                                         int pageSize) : IQuery<Result<PagedList<DoctorPositionDTO>>>;
+
+public class GetAllDoctorPositionQueryHandler : IQueryHandler<GetAllDoctorPositionsQuery, Result<PagedList<DoctorPositionDTO>>>
+{
+    private readonly IDoctorPositionRepository _doctorPositionRepository;
+
+    public GetAllDoctorPositionQueryHandler(IDoctorPositionRepository doctorPositionRepository)
+    {
+        _doctorPositionRepository = doctorPositionRepository;
+    }
+
+    public async Task<Result<PagedList<DoctorPositionDTO>>> Handle(GetAllDoctorPositionsQuery request, CancellationToken cancellationToken)
+    {
+       PagedList<DoctorPositionDTO> result = await _doctorPositionRepository.GetAllDoctorPositions(request.name,request.sortColumn,request.sortOrder,request.page,request.pageSize);
+
+        return Result<PagedList<DoctorPositionDTO>>.Sucess(result);
+    }
+}
