bfb21d9b3ff768ba6a33493b08531ca67f1b40ff
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-03-25 19:51:12 -0400

    Create DeletepatientValidator

---
+++ Clinic.Business/Patients/Commands/DeletePatient/DeletePatientValidator.cs
@@ -0,0 +1,15 @@
+using Clinic.Business.Patients.Commands;
+using Clinic.Business.Patients.Commands.DeletePatient;
+using FluentValidation;
+
+namespace Clinic.Business.Patients.Validators
+{
+    public class DeletePatientValidator : AbstractValidator<DeletePatientCommand>
+    {
+        public DeletePatientValidator()
+        {
+            RuleFor(command => command.patientId)
+                .NotEmpty().WithMessage("Patient ID is required.");
+        }
+    }
+}
