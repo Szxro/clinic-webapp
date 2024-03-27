f627326d4bab56696daa96607ae0db86e353db83
Szxro <szxro@gmail.com>
2024-03-26 20:57:32 -0400

    Reduce nesting

--- Clinic.Api/Controllers/PatientController.cs
+++ Clinic.Api/Controllers/PatientController.cs
@@ -8,54 +8,53 @@ using Clinic.Data.Entities.Common.Primitives;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Clinic.Api.Controllers
+namespace Clinic.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PatientController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PatientController : ControllerBase
+    private readonly ISender _sender;
+
+    public PatientController(ISender sender)
     {
-        private readonly ISender _sender;
+        _sender = sender;
+    }
 
-        public PatientController(ISender sender)
-        {
-            _sender = sender;
-        }
+    [HttpPost("patient/create")]
+    public async Task<ActionResult> CreatePatient(CreatePatientCommand createPatient)
+    {
+        Result result = await _sender.Send(createPatient);
 
-        [HttpPost("patient/create")]
-        public async Task<ActionResult> CreatePatient(CreatePatientCommand createPatient)
-        {
-            Result result = await _sender.Send(createPatient);
+        return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+    }
 
-            return result.IsSuccess ? NoContent() : result.ToProblemDetails();
-        }
+    [HttpPut("patient/update")]
+    public async Task<ActionResult> UpdatePatient(UpdatePatientCommand updatePatient)
+    {
+        Result result = await _sender.Send(updatePatient);
 
-        [HttpPut("patient/update")]
-        public async Task<ActionResult> UpdatePatient(UpdatePatientCommand updatePatient)
-        {
-            Result result = await _sender.Send(updatePatient);
+        return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+    }
 
-            return result.IsSuccess ? NoContent() : result.ToProblemDetails();
-        }
+    [HttpDelete("patient/remove")]
+    public async Task<ActionResult> DeletePatient(DeletePatientCommand deletePatient)
+    {
+        Result result = await _sender.Send(deletePatient);
 
-        [HttpDelete("patient/remove")]
-        public async Task<ActionResult> DeletePatient(DeletePatientCommand deletePatient)
-        {
-            Result result = await _sender.Send(deletePatient);
+        return result.IsSuccess ? NoContent() : result.ToProblemDetails();
+    }
+  
 
-            return result.IsSuccess ? NoContent() : result.ToProblemDetails();
-        }
-      
+    [HttpGet("patient/all")]
+    public async Task<ActionResult<PagedList<PatientResponse>>> GetPatientInformation(string? name,
+                                                                                 string? sortColumn,
+                                                                                 string? sortOrder,
+                                                                                 int page,
+                                                                                 int pageSize)
+    {
+        Result<PagedList<PatientResponse>> result = await _sender.Send(new GetPatientInformationQuery(name, sortColumn, sortOrder, page, pageSize));
 
-        [HttpGet("patient/all")]
-        public async Task<ActionResult<PagedList<PatientDto>>> GetPatientInformation(string? name,
-                                                                                     string? sortColumn,
-                                                                                     string? sortOrder,
-                                                                                     int page,
-                                                                                     int pageSize)
-        {
-            Result<PagedList<PatientDto>> result = await _sender.Send(new GetPatientInformationQuery(name, sortColumn, sortOrder, page, pageSize));
-
-            return result.IsSuccess ? Ok(result.Data) : result.ToProblemDetails();
-        }
+        return result.IsSuccess ? Ok(result.Data) : result.ToProblemDetails();
     }
 }
