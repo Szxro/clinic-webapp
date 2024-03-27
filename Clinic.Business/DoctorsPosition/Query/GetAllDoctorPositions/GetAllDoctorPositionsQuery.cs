958fcdb5a4304a34162c6c9176ac8044efc7d6ac
Szxro <szxro@gmail.com>
2024-03-26 20:59:36 -0400

    Change return type from DTO to Response

--- Clinic.Business/DoctorsPosition/Query/GetAllDoctorPositions/GetAllDoctorPositionsQuery.cs
+++ Clinic.Business/DoctorsPosition/Query/GetAllDoctorPositions/GetAllDoctorPositionsQuery.cs
@@ -8,9 +8,9 @@ public record GetAllDoctorPositionsQuery(string? name,
                                          string? sortColumn,
                                          string? sortOrder,
                                          int page,
-                                         int pageSize) : IQuery<Result<PagedList<DoctorPositionDTO>>>;
+                                         int pageSize) : IQuery<Result<PagedList<DoctorPositionResponse>>>;
 
-public class GetAllDoctorPositionQueryHandler : IQueryHandler<GetAllDoctorPositionsQuery, Result<PagedList<DoctorPositionDTO>>>
+public class GetAllDoctorPositionQueryHandler : IQueryHandler<GetAllDoctorPositionsQuery, Result<PagedList<DoctorPositionResponse>>>
 {
     private readonly IDoctorPositionRepository _doctorPositionRepository;
 
@@ -19,10 +19,10 @@ public class GetAllDoctorPositionQueryHandler : IQueryHandler<GetAllDoctorPositi
         _doctorPositionRepository = doctorPositionRepository;
     }
 
-    public async Task<Result<PagedList<DoctorPositionDTO>>> Handle(GetAllDoctorPositionsQuery request, CancellationToken cancellationToken)
+    public async Task<Result<PagedList<DoctorPositionResponse>>> Handle(GetAllDoctorPositionsQuery request, CancellationToken cancellationToken)
     {
-       PagedList<DoctorPositionDTO> result = await _doctorPositionRepository.GetAllDoctorPositions(request.name,request.sortColumn,request.sortOrder,request.page,request.pageSize);
+       PagedList<DoctorPositionResponse> result = await _doctorPositionRepository.GetAllDoctorPositions(request.name,request.sortColumn,request.sortOrder,request.page,request.pageSize);
 
-        return Result<PagedList<DoctorPositionDTO>>.Sucess(result);
+        return Result<PagedList<DoctorPositionResponse>>.Sucess(result);
     }
 }
