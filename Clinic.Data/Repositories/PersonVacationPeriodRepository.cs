c0be599b2062499b713243a6f7e86da3026075c9
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-03-30 15:47:23 -0400

    Create PersonVacationPeriodRepository

---
+++ Clinic.Data/Repositories/PersonVacationPeriodRepository.cs
@@ -0,0 +1,33 @@
+﻿using Clinic.Data.Common;
+using Clinic.Data.Contracts;
+using Clinic.Data.Entities;
+using Clinic.Data.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Data.Repositories;
+
+public class PersonVacationPeriodRepository : GenericRepository<PersonVacationPeriod>, IPersonVacationPeriodRepository
+{
+    public PersonVacationPeriodRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<PersonVacationPeriod?> GetByIdWithDetailsAsync(int id)
+    {
+        return await _dbContext.Set<PersonVacationPeriod>()
+            .Include(pvp => pvp.Person)
+            .Include(pvp => pvp.VacationPeriodStatus)
+            .FirstOrDefaultAsync(pvp => pvp.Id == id);
+    }
+
+    public async Task<List<PersonVacationPeriod>> GetByPersonIdAsync(int personId)
+    {
+        return await _dbContext.Set<PersonVacationPeriod>()
+            .Include(pvp => pvp.Person)
+            .Include(pvp => pvp.VacationPeriodStatus)
+            .Where(pvp => pvp.PersonId == personId)
+            .ToListAsync();
+    }
+
+
+}
