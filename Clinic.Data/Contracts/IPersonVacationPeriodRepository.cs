98891448e1bb904e8e3b42a421095dd7920d8194
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-03-30 14:58:31 -0400

    Create IPersonVacationPeriodRepository

---
+++ Clinic.Data/Contracts/IPersonVacationPeriodRepository.cs
@@ -0,0 +1,10 @@
+﻿using Clinic.Data.Common;
+using Clinic.Data.Entities;
+
+namespace Clinic.Data.Contracts;
+
+public interface IPersonVacationPeriodRepository 
+{
+    Task<PersonVacationPeriod?> GetByIdWithDetailsAsync(int id);
+    Task<List<PersonVacationPeriod>> GetByPersonIdAsync(int personId);
+}
