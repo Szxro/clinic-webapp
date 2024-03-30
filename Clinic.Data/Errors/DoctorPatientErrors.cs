7866beab999c462de418b2387e48df088c35e542
Szxro <szxro@gmail.com>
2024-03-30 13:06:38 -0400

    Add errors for the doctor patient entity

---
+++ Clinic.Data/Errors/DoctorPatientErrors.cs
@@ -0,0 +1,9 @@
+﻿using Clinic.Data.Entities.Common.Primitives;
+
+namespace Clinic.Data.Errors;
+
+public static class DoctorPatientErrors
+{
+    public static readonly Error AlreadyAssociatedPatient
+         = Error.Conflit("DoctorPatient.Conflict", "Patient or the Doctor is already associated");
+}
