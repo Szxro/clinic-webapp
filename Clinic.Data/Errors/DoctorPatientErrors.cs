a876e07e15c39ca5ec352d4cc8de3e5306e5b180
Szxro <szxro@gmail.com>
2024-03-30 13:11:30 -0400

    Make some arrangements to the doctor patient errors

--- Clinic.Data/Errors/DoctorPatientErrors.cs
+++ Clinic.Data/Errors/DoctorPatientErrors.cs
@@ -5,5 +5,5 @@ namespace Clinic.Data.Errors;
 public static class DoctorPatientErrors
 {
     public static readonly Error AlreadyAssociatedPatient
-         = Error.Conflit("DoctorPatient.Conflict", "Patient or the Doctor is already associated");
+         = Error.Conflit("DoctorPatient.Conflict", "The patient or the doctor is already associated");
 }
