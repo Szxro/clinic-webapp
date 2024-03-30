304bfdd61b8a962a6fed33cca9a941b67316f273
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-03-30 19:14:27 -0400

    Added GetPersonVacationPeriodsInformation method to interface and repo

--- Clinic.Data/Contracts/IPersonVacationPeriodRepository.cs
+++ Clinic.Data/Contracts/IPersonVacationPeriodRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Clinic.Data.Common;
+using Clinic.Data.DTOs;
 using Clinic.Data.Entities;
+using Clinic.Data.Entities.Common.Primitives;
 
 namespace Clinic.Data.Contracts;
 
@@ -10,4 +12,5 @@ public interface IPersonVacationPeriodRepository
     Task AddPersonVacationPeriodAsync(PersonVacationPeriod personVacationPeriod);
     void UpdatePersonVacationPeriod(PersonVacationPeriod personVacationPeriod);
     void DeletePersonVacationPeriod(PersonVacationPeriod personVacationPeriod);
+    Task<PagedList<PersonVacationPeriodResponse>> GetPersonVacationPeriodsInformation(int personId, string? sortColumn, string? sortOrder, int page, int pageSize);
 }
--- Clinic.Data/Repositories/PersonVacationPeriodRepository.cs
+++ Clinic.Data/Repositories/PersonVacationPeriodRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Clinic.Data.Common;
 using Clinic.Data.Contracts;
+using Clinic.Data.DTOs;
 using Clinic.Data.Entities;
+using Clinic.Data.Entities.Common.Primitives;
 using Clinic.Data.Persistence;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Clinic.Data.Repositories;
 
@@ -44,4 +47,48 @@ public class PersonVacationPeriodRepository : GenericRepository<PersonVacationPe
         _dbContext.Set<PersonVacationPeriod>().Remove(personVacationPeriod);
     }
 
+    public async Task<PagedList<PersonVacationPeriodResponse>> GetPersonVacationPeriodsInformation(int personId, string? sortColumn, string? sortOrder, int page, int pageSize)
+    {
+        IQueryable<PersonVacationPeriod> queryable = _dbContext.Set<PersonVacationPeriod>()
+            .Include(pvp => pvp.Person)
+            .Include(pvp => pvp.VacationPeriodStatus)
+            .Where(pvp => pvp.PersonId == personId);
+
+        if (sortOrder?.ToLower() == "desc")
+        {
+            queryable = queryable.OrderByDescending(GetSortProperty(sortColumn));
+        }
+        else
+        {
+            queryable = queryable.OrderBy(GetSortProperty(sortColumn));
+        }
+
+        IQueryable<PersonVacationPeriodResponse> personVacationPeriods = queryable
+            .AsNoTracking()
+            .Select(pvp =>
+                new PersonVacationPeriodResponse
+                {
+                    Id = pvp.Id,
+                    PersonId = pvp.PersonId,
+                    PersonName = pvp.Person.Name,
+                    StartDate = pvp.StartDate.ToString("dd-MM-yyyy"),
+                    EndDate = pvp.EndDate.HasValue ? pvp.EndDate.Value.ToString("dd-MM-yyyy") : null,
+                    VacationPeriodStatusId = pvp.VacationPeriodStatusId,
+                    VacationPeriodStatusName = pvp.VacationPeriodStatus.StatusName
+                });
+
+        return await MakePagedList(personVacationPeriods, page, pageSize);
+    }
+
+    private static Expression<Func<PersonVacationPeriod, object>> GetSortProperty(string? sortColumn)
+    {
+        return sortColumn?.ToLower() switch
+        {
+            "startdate" => pvp => pvp.StartDate,
+            "enddate" => pvp => pvp.EndDate ?? DateTime.MinValue,
+            "vacationperiodstatusname" => pvp => pvp.VacationPeriodStatus.StatusName,
+            _ => pvp => pvp.Id
+        };
+    }
 }
+
