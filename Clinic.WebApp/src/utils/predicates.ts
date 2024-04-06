4fc525df55c67c2e28405337e26c87c3b7e4ea86
Szxro <szxro@gmail.com>
2024-04-05 22:08:12 -0400

    Add some predicates to determined the type

---
+++ Clinic.WebApp/src/utils/predicates.ts
@@ -0,0 +1,14 @@
+import { ProblemDetails } from "../models/problem-details.model";
+import { FetchWrapperError } from "../models/wrapper.model";
+
+export const isProblemDetails = (
+  response: unknown
+): response is ProblemDetails => {
+  return (response as ProblemDetails).status !== undefined;
+};
+
+export const isWrapperError = (
+  response: unknown
+): response is FetchWrapperError => {
+  return (response as FetchWrapperError).message !== undefined;
+};
