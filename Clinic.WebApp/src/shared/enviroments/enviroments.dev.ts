7f6fc6e8687ee4735690ab008cc8429fe9cd3a1e
Szxro <szxro@gmail.com>
2024-04-18 21:35:49 -0400

    Add environment object to contain the urls for the necessaries requests

---
+++ Clinic.WebApp/src/shared/enviroments/enviroments.dev.ts
@@ -0,0 +1,10 @@
+export const enviroments = {
+  getAllDoctorPosition: "https://localhost:7252/api/v1/doctorposition/all",
+  createDoctorPosition: "https://localhost:7252/api/v1/doctorposition/create",
+  createDoctor: "https://localhost:7252/api/v1/doctor/create",
+  deleteDoctor: "https://localhost:7252/api/v1/doctor/remove",
+  updateDoctor: "https://localhost:7252/api/v1/doctor/update",
+  createPatient: "https://localhost:7252/api/v1/patient/create",
+  updatePatient: "https://localhost:7252/api/v1/patient/update",
+  deletePatient: "https://localhost:7252/api/v1/patient/remove",
+};
